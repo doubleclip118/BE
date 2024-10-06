3cad9655b5c458d34bc84e802544b71890d32994
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-10-06 15:42:30 +0900

    init : 초기설정

---
+++ src/test/java/PNUMEAT/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package PNUMEAT.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
