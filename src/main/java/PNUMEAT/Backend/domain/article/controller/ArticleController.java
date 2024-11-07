c51598c8af12f588ab577fdedd84fe2178779525
wvssm <neoramiregi@gmail.com>
2024-11-07 23:02:38 +0900

    feat: 수정 폼 수정 (이미지도 수정할 수 있도록)

--- src/main/java/PNUMEAT/Backend/domain/article/controller/ArticleController.java
+++ src/main/java/PNUMEAT/Backend/domain/article/controller/ArticleController.java
@@ -86,7 +86,7 @@ public class ArticleController {
 
     @PostMapping("/{id}")
     public String updateArticle(@PathVariable Long id,
-                                @RequestBody ArticleRequest articleRequest,
+                                @ModelAttribute ArticleRequest articleRequest,
                                 @LoginMember Member member,
                                 HttpServletRequest request,
                                 @RequestParam("upload") MultipartFile multipartFile) {
