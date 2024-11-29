48002e2934be7fca97215c66e5435356d4e09a71
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-11-29 22:47:40 +0900

    fix : "로직변경"

--- src/main/java/PNUMEAT/Backend/domain/article/service/ArticleService.java
+++ src/main/java/PNUMEAT/Backend/domain/article/service/ArticleService.java
@@ -1,119 +1,119 @@
-package PNUMEAT.Backend.domain.article.service;
-
-
-import PNUMEAT.Backend.domain.article.dto.request.ArticleRequest;
-import PNUMEAT.Backend.domain.article.entity.Article;
-import PNUMEAT.Backend.domain.article.enums.Category;
-import PNUMEAT.Backend.domain.article.repository.ArticleRepository;
-import PNUMEAT.Backend.domain.auth.entity.User;
-import PNUMEAT.Backend.global.error.Team24Exception;
-import PNUMEAT.Backend.global.images.ImageService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
-
-import static PNUMEAT.Backend.global.error.ErrorCode.*;
-
-@Service
-@Slf4j
-public class ArticleService {
-    private final ImageService imageService;
-    private final ArticleRepository articleRepository;
-
-    public ArticleService(ImageService imageService, ArticleRepository articleRepository) {
-        this.imageService = imageService;
-        this.articleRepository = articleRepository;
-    }
-
-    @Transactional
-    public Article save(ArticleRequest articleRequest, User user, MultipartFile multipartFile){
-
-        Article article = toEntity(articleRequest, user,null);
-
-        Article savedArticle = articleRepository.save(article);
-
-        Long articleId = article.getArticleId();
-
-        article.insertimgUrl(imageService.imageload(multipartFile, articleId));
-
-        return savedArticle;
-    }
-
-    @Transactional(readOnly = true)
-    public Article findById(Long id, User user){
-        Article article =  articleRepository.findById(id)
-                .orElseThrow(() -> new Team24Exception(ARTICLE_NOT_FOUND_ERROR));
-
-        if(!user.getRole().equals("ROLE_ADMIN")){
-            if(!article.getUser().getId().equals(user.getId())){
-                throw new Team24Exception(ARTICLE_FORBIDDEN_ERROR);
-            }
-        }
-        return article;
-    }
-
-    @Transactional(readOnly = true)
-    public List<Article> findAllArticles(){
-        return articleRepository.findAll();
-    }
-
-    @Transactional(readOnly = true)
-    public List<Article> findByMemberId(Long memberId){
-        return articleRepository.findByUserId(memberId);
-    }
-
-    @Transactional
-    public void deleteById(Long id, User user){
-        Article article = articleRepository.findById(id)
-                .orElseThrow(()-> new Team24Exception(ARTICLE_NOT_FOUND_ERROR));
-
-        if(!user.getRole().equals("ROLE_ADMIN")){
-            if(!article.getUser().getId().equals(user.getId())){
-                throw new Team24Exception(ARTICLE_FORBIDDEN_ERROR);
-            }
-        }
-
-        imageService.deleteImageByUrl(article.getImage());
-
-        articleRepository.deleteById(id);
-    }
-
-    @Transactional
-    public void updateById(Long id, ArticleRequest articleRequest, User user, MultipartFile multipartFile){
-        Article article = articleRepository.findById(id)
-                .orElseThrow(()-> new Team24Exception(ARTICLE_NOT_FOUND_ERROR));
-
-        if(!user.getRole().equals("ROLE_ADMIN")){
-            if(!article.getUser().getId().equals(user.getId())){
-                throw new Team24Exception(ARTICLE_FORBIDDEN_ERROR);
-            }
-        }
-
-        String imageload = article.getImage();
-        if (multipartFile != null && !multipartFile.isEmpty()) {
-            imageload = imageService.imageload(multipartFile, article.getArticleId());
-        }
-
-        article.updateArticle(articleRequest.title(), articleRequest.content(), articleRequest.category(), imageload);
-    }
-
-    @Transactional(readOnly = true)
-    public List<Article> findByCategory(Category category){
-        return articleRepository.findByCategory(category);
-    }
-
-
-    public Article toEntity(ArticleRequest articleRequest, User user, String imgUrl) {
-        return Article.builder()
-                .user(user)
-                .title(articleRequest.title())
-                .content(articleRequest.content())
-                .category(articleRequest.category())
-                .image(imgUrl)
-                .deleted(false)
-                .build();
-    }
-}
+//package PNUMEAT.Backend.domain.article.service;
+//
+//
+//import PNUMEAT.Backend.domain.article.dto.request.ArticleRequest;
+//import PNUMEAT.Backend.domain.article.entity.Article;
+//import PNUMEAT.Backend.domain.article.enums.Category;
+//import PNUMEAT.Backend.domain.article.repository.ArticleRepository;
+//import PNUMEAT.Backend.domain.auth.entity.User;
+//import PNUMEAT.Backend.global.error.Team24Exception;
+//import PNUMEAT.Backend.global.images.ImageService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//import org.springframework.transaction.annotation.Transactional;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import static PNUMEAT.Backend.global.error.ErrorCode.*;
+//
+//@Service
+//@Slf4j
+//public class ArticleService {
+//    private final ImageService imageService;
+//    private final ArticleRepository articleRepository;
+//
+//    public ArticleService(ImageService imageService, ArticleRepository articleRepository) {
+//        this.imageService = imageService;
+//        this.articleRepository = articleRepository;
+//    }
+//
+//    @Transactional
+//    public Article save(ArticleRequest articleRequest, User user, MultipartFile multipartFile){
+//
+//        Article article = toEntity(articleRequest, user,null);
+//
+//        Article savedArticle = articleRepository.save(article);
+//
+//        Long articleId = article.getArticleId();
+//
+//        article.insertimgUrl(imageService.imageUpload(multipartFile, articleId));
+//
+//        return savedArticle;
+//    }
+//
+//    @Transactional(readOnly = true)
+//    public Article findById(Long id, User user){
+//        Article article =  articleRepository.findById(id)
+//                .orElseThrow(() -> new Team24Exception(ARTICLE_NOT_FOUND_ERROR));
+//
+//        if(!user.getRole().equals("ROLE_ADMIN")){
+//            if(!article.getUser().getId().equals(user.getId())){
+//                throw new Team24Exception(ARTICLE_FORBIDDEN_ERROR);
+//            }
+//        }
+//        return article;
+//    }
+//
+//    @Transactional(readOnly = true)
+//    public List<Article> findAllArticles(){
+//        return articleRepository.findAll();
+//    }
+//
+//    @Transactional(readOnly = true)
+//    public List<Article> findByMemberId(Long memberId){
+//        return articleRepository.findByUserId(memberId);
+//    }
+//
+//    @Transactional
+//    public void deleteById(Long id, User user){
+//        Article article = articleRepository.findById(id)
+//                .orElseThrow(()-> new Team24Exception(ARTICLE_NOT_FOUND_ERROR));
+//
+//        if(!user.getRole().equals("ROLE_ADMIN")){
+//            if(!article.getUser().getId().equals(user.getId())){
+//                throw new Team24Exception(ARTICLE_FORBIDDEN_ERROR);
+//            }
+//        }
+//
+//        imageService.deleteImageByUrl(article.getImage());
+//
+//        articleRepository.deleteById(id);
+//    }
+//
+//    @Transactional
+//    public void updateById(Long id, ArticleRequest articleRequest, User user, MultipartFile multipartFile){
+//        Article article = articleRepository.findById(id)
+//                .orElseThrow(()-> new Team24Exception(ARTICLE_NOT_FOUND_ERROR));
+//
+//        if(!user.getRole().equals("ROLE_ADMIN")){
+//            if(!article.getUser().getId().equals(user.getId())){
+//                throw new Team24Exception(ARTICLE_FORBIDDEN_ERROR);
+//            }
+//        }
+//
+//        String imageload = article.getImage();
+//        if (multipartFile != null && !multipartFile.isEmpty()) {
+//            imageload = imageService.imageUpload(multipartFile, article.getArticleId());
+//        }
+//
+//        article.updateArticle(articleRequest.title(), articleRequest.content(), articleRequest.category(), imageload);
+//    }
+//
+//    @Transactional(readOnly = true)
+//    public List<Article> findByCategory(Category category){
+//        return articleRepository.findByCategory(category);
+//    }
+//
+//
+//    public Article toEntity(ArticleRequest articleRequest, User user, String imgUrl) {
+//        return Article.builder()
+//                .user(user)
+//                .title(articleRequest.title())
+//                .content(articleRequest.content())
+//                .category(articleRequest.category())
+//                .image(imgUrl)
+//                .deleted(false)
+//                .build();
+//    }
+//}
--- src/main/java/PNUMEAT/Backend/global/images/ImageService.java
+++ src/main/java/PNUMEAT/Backend/global/images/ImageService.java
@@ -2,6 +2,7 @@ package PNUMEAT.Backend.global.images;
 
 import PNUMEAT.Backend.global.error.images.ImageFileDeleteException;
 import PNUMEAT.Backend.global.error.images.ImageFileUploadException;
+import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,16 +14,18 @@ import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 public class ImageService {
     private final S3Client s3Client;
     private final AwsProperties awsProperties;
+
+    private static final String LOCAL_LOCATION = "/Users/pakjeongwoo/Downloads/";
     public ImageService(S3Client s3Client, AwsProperties awsProperties) {
         this.s3Client = s3Client;
         this.awsProperties = awsProperties;
     }
 
-    public String imageload(MultipartFile multipartFile, Long id) {
+    public String imageUpload(MultipartFile multipartFile) {
         String s3Url;
         try {
-            String localLocation = "/Users/pakjeongwoo/4-2/pnu_meat/BE/";
-            Image image = new Image(multipartFile, id, localLocation);
+            String uuid = UUID.randomUUID().toString();
+            Image image = new Image(multipartFile, uuid, LOCAL_LOCATION);
             s3Url = image.uploadToS3(s3Client, awsProperties.getBucket(), awsProperties.getRegion());
 
             if (!image.deleteLocalFile()) {
@@ -34,6 +37,24 @@ public class ImageService {
         }
 
         return s3Url;
+    }
+
+    public String imageUpdate(MultipartFile multipartFile, String imageUrl) {
+        String updatedS3Url;
+        try {
+            String key = imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
+            Image image = new Image(multipartFile, key, LOCAL_LOCATION);
+            updatedS3Url = image.uploadToS3(s3Client, awsProperties.getBucket(), awsProperties.getRegion());
+
+            if (!image.deleteLocalFile()) {
+                log.error("로컬 파일 삭제 실패: {}", image.getLocalPath());
+            }
+        } catch (Exception e) {
+            log.error("이미지 업데이트 중 오류 발생: {}", e.getMessage());
+            throw new ImageFileUploadException();
+        }
+
+        return updatedS3Url;
     }
 
     public void deleteImageByUrl(String s3Url) {
