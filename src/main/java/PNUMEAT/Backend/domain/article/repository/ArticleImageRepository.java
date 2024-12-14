98c9c7a038183086bc4f71c5dbf5c480be49d0c2
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-12-14 22:55:11 +0900

    feat : 게시물 이미지 레포지토리 생성

---
+++ src/main/java/PNUMEAT/Backend/domain/article/repository/ArticleImageRepository.java
@@ -0,0 +1,8 @@
+package PNUMEAT.Backend.domain.article.repository;
+
+import PNUMEAT.Backend.domain.article.entity.ArticleImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleImageRepository extends JpaRepository<ArticleImage,Long> {
+
+}
