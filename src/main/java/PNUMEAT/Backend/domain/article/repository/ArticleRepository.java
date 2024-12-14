d1102382c4ab66b6fa75b6aa85bcbe61a3d46cbc
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-12-14 22:55:31 +0900

    feat : 게시물 레포지토리 생성

--- src/main/java/PNUMEAT/Backend/domain/article/repository/ArticleRepository.java
+++ src/main/java/PNUMEAT/Backend/domain/article/repository/ArticleRepository.java
@@ -1,8 +1,9 @@
 package PNUMEAT.Backend.domain.article.repository;
 
 import PNUMEAT.Backend.domain.article.entity.Article;
-import PNUMEAT.Backend.domain.article.enums.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +12,14 @@ import java.util.Optional;
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     Optional<Article> findByArticleId(Long articleId);
-    List<Article> findByMemberId(Long memberId);
-    List<Article> findByCategory(Category category);
+    @Query("SELECT a FROM Article a LEFT JOIN FETCH a.images WHERE a.member.id = :memberId")
+    List<Article> findByMemberIdWithImages(@Param("memberId") Long memberId);
+
+    @Query("SELECT a FROM Article a LEFT JOIN FETCH a.images WHERE a.team.teamId = :teamId")
+    List<Article> findByTeamTeamIdWithImages(@Param("teamId") Long teamId);
+
+    @Query("SELECT a FROM Article a LEFT JOIN FETCH a.images WHERE a.articleId = :articleId")
+    Optional<Article> findByIdWithImages(@Param("articleId") Long articleId);
+
+
 }
