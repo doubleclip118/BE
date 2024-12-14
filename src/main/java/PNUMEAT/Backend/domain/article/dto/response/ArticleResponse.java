8f3758010140707355460805c7d54aa30c9fc7ec
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-12-14 22:55:48 +0900

    feat : 게시물 response dto 생성

--- src/main/java/PNUMEAT/Backend/domain/article/dto/response/ArticleResponse.java
+++ src/main/java/PNUMEAT/Backend/domain/article/dto/response/ArticleResponse.java
@@ -1,17 +1,25 @@
 package PNUMEAT.Backend.domain.article.dto.response;
 
 import PNUMEAT.Backend.domain.article.entity.Article;
-import PNUMEAT.Backend.domain.article.enums.Category;
+import PNUMEAT.Backend.domain.article.enums.ArticleCategory;
+import java.time.LocalDateTime;
 
 public record ArticleResponse(
-        Long articleId,
-        String title,
-        String content,
-        Category category,
-        String image,
-        String createdAt
+    Long articleId,
+    String articleTitle,
+    String articleBody,
+    LocalDateTime updatedDate,
+    ArticleCategory articleCategory,
+    String imageUrl
 ) {
-    public static ArticleResponse of(Article article,String createdAt){
-        return new ArticleResponse(article.getArticleId(), article.getTitle(), article.getContent(), article.getCategory(), article.getImage(), createdAt);
+    public static ArticleResponse of(Article article) {
+        return new ArticleResponse(
+            article.getArticleId(),
+            article.getArticleTitle(),
+            article.getArticleBody(),
+            article.getUpdatedDate(),
+            article.getArticleCategory(),
+            article.getImages().get(0).getImageUrl()
+        );
     }
 }
